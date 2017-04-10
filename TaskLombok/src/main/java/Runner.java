72e021da11636f2603806126beabe0a73a5436f0
Oleksandr Khodakovskyi <Oleksandr_Khodakovskyi@epam.com>
2017-04-10 20:23:08 +0300

    TaskLombok module added

---
+++ TaskLombok/src/main/java/Runner.java
@@ -0,0 +1,15 @@
+import java.sql.Struct;
+
+public class Runner {
+    public static void main(String[] args) {
+        Student.StudentBuilder builder = new Student.StudentBuilder();
+        Student student =  builder
+                .age(10)
+                .marks(new int[]{10, 11, 12})
+                .name("Alex")
+                .build();
+
+        System.out.println(student);
+
+    }
+}
